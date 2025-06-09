5f571cdc68cdcc1160a36fca25eadfa4eceb1591
Nil Farrés Soler <Nil.Farres@autonoma.cat>
2025-06-09 10:21:08 +0200

    Update mapa_preds.bat

--- mapa_preds.bat
+++ mapa_preds.bat
@@ -5,13 +5,13 @@ REM ============================================================
 
 REM EXECUTAR L'SCRIPT AMB ELS PARÀMETRES QUE ES VULGUI
 python mapa_preds.py ^
-    --ncfile "C:/Users/nfarres/Documents/TFG/models/exec/predictions_meteographpc_test.nc" ^
+    --ncfile "C:/Users/nfarres/Documents/TFG/models/model_curt_termini/predictions_meteographpc_test_model_curt_termini.nc" ^
     --time 1 ^
     --variable Temp ^
     --interp none ^
     --resol 500 ^
     --maxdist 120 ^
-    --output "C:/Users/nfarres/Documents/TFG/models/exec/prediccions-temp/mapa_pred" ^
+    --output "C:/Users/nfarres/Documents/TFG/models/model_curt_termini/prediccions-temp/mapa_pred" ^
     --all_times
 
 pause
